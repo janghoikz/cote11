482e339843921d10776256954e0f39a673957b33
ohne <ghdlr785@gmail.com>
2022-06-21 20:10:40 +0900

    초기 커밋.

---
+++ cote11/cote11.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) 
+{
+    long long answer = 0;
+    vector<long long> num;
+    //백터에다 한자리씩 넣기
+    while (n != 0) 
+    {
+        num.push_back(n % 10);
+        n /= 10;
+    }
+    // 백터 오름차순 정렬
+    sort(num.begin(), num.end());
+    for (int i = 0; i < num.size(); i++)
+    {
+        if (i == 0)
+        {
+            answer = num[i];
+        }
+        else
+        {
+            int a = 0;
+            a = pow(10, i);
+            answer = answer + (num[i] * a);
+        }
+    }
+
+    cout << answer << endl;
+    return answer;
+}
+
+int main()
+{
+    long long ab = 157;
+    solution(ab);
+
+}
+
